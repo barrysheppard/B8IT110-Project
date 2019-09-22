be8b53f31b73b26cbce56674c8a85714837cb1c0
Barry Sheppard <barrysheppard@gmail.com>
2019-09-22 22:41:42 +0100

    Restructured into functions

--- load.py
+++ load.py
@@ -5,33 +5,13 @@
 # Task: Load keyforge decks from https://www.keyforgegame.com       #
 #####################################################################
 
-
-#####################################################################
-# Import                                                            #
-#####################################################################
-import requests
-import json
-#####################################################################
-# Functions                                                         #
-#####################################################################
-
-
-#####################################################################
-# Code                                                              #
-#####################################################################
-
-# This will load the very first deck and only that deck
-deck_number = "1"
-url = "https://www.keyforgegame.com/api/decks/?page=" + deck_number + "&page_size=1"
-r = requests.get(url)
-data = json.loads(r.content.decode())
-# print(data)
-
+# Decks are listed on the keyforgegame.com website.
+# The API there returns details of a single deck
 # Each deck has the following
 # data - This is the dictionary of all the following
 #    name - This is the name of the deck e.g. 'Dr. "The Old" Jeffries'
 #    expansion - This is the release wave the deck was in
-#    power_level - This is calculated 'strength' of the deck based on wis
+#    power_level - This is calculated 'strength' of the deck based on wins
 #    chains - This is a penalty in play for being poewrful
 #    wins - The number of wins in tournaments
 #    losses - The number of losses in tournaments
@@ -41,17 +21,69 @@ data = json.loads(r.content.decode())
 #    notes - When you're logged in, your notes appear here
 #    is_my_favorite - When you're logged in, you can flag this
 #    is_on_my_watchlist - When you're logged in, you can flag this
-#    casual_wins - This is the number of casual wins as recorded by owner
-#    casual_losses - This is the number of casual losses as recorded by owner
+#    casual_wins - This is the number of casual wins as recorded
+#    casual_losses - This is the number of casual losses as recorded
 #    set_era_cards - Some cards come from different expansions
 # _links - This has some extra data, see below
 #    houses - Each deck has 3 houses, every card is in one of those 3
 # count - This is the total number of decks registered in the database
 
+#####################################################################
+# Import                                                            #
+#####################################################################
+import requests
+import json
 
-print('Total Decks:', data['count'])
-print('Deck Name:', data['data'][0]['name'])
-house1 = data['_linked']['houses'][0]['name']
-house2 = data['_linked']['houses'][1]['name']
-house3 = data['_linked']['houses'][2]['name']
-print('Houses:', house1, house2, house3)
+#####################################################################
+# Functions                                                         #
+#####################################################################
+
+
+def load_deck(deck_number):
+    """Return details of deck from keyforgegame.com website"""
+    # Load the deck in json format
+    website = "https://www.keyforgegame.com/api/decks/?page="
+    url = website + deck_number + "&page_size=1"
+    r = requests.get(url)
+    data = json.loads(r.content.decode())
+    # Pull out the data of interest
+    deck_name = data['data'][0]['name']
+    deck_house1 = data['_linked']['houses'][0]['name']
+    deck_house2 = data['_linked']['houses'][1]['name']
+    deck_house3 = data['_linked']['houses'][2]['name']
+    deck_wins = data['data'][0]['wins']
+    deck_losses = data['data'][0]['losses']
+    deck_expansion = data['data'][0]['expansion']
+    deck_card_list = data['data'][0]['cards']
+    return [deck_name, deck_house1, deck_house2, deck_house3, deck_wins,
+            deck_losses, deck_expansion, deck_card_list]
+
+
+def total_decks():
+    """Returns the total number of decks registered on keyforgegame.com"""
+    # We load the details of deck number 1
+    # Every deck detail includes the number of all decks registered
+    website = "https://www.keyforgegame.com/api/decks/?page="
+    url = website + "1" + "&page_size=1"
+    r = requests.get(url)
+    data = json.loads(r.content.decode())
+    return data['count']
+
+
+#####################################################################
+# Code                                                              #
+#####################################################################
+
+if __name__ == '__main__':
+
+    # This will load the very first deck and only that deck
+    deck_number = "1"
+    deck = load_deck(deck_number)
+
+    print('Total Decks:', total_decks())
+    print('Deck Name:', deck[0])
+    print('Houses:', deck[1], deck[2], deck[3])
+    print('Wins:', deck[4])
+    print('Losses:', deck[5])
+    print('Expansion:', deck[6])
+    print('Cards:', deck[7])
