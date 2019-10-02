335c924f1ff34daa71169e7bd9acb07c1c0eb203
Barry Sheppard <barrysheppard@gmail.com>
2019-10-02 19:36:56 +0100

    Update of unittests for new functions, additional asserts to be added later

--- test_load.py
+++ test_load.py
@@ -10,36 +10,47 @@
 # Import                                                                      #
 ###############################################################################
 import unittest
-from load import load_deck
+from load import load_decks
 from load import total_decks
+from load import decode_cards
+from load import decode_decks
+
 
 ###############################################################################
-# Code for when file is run from command line                                 #
+# Functions                                                                   #
 ###############################################################################
 
 
 # test the load functionality
 class TestLoad(unittest.TestCase):
 
-    def test_load_deck(self):
-        deck = load_deck("1")
-        self.assertEqual('Dr. "The Old" Jeffries', deck[0])
-        self.assertEqual(0, deck[1])
-        self.assertEqual(0, deck[2])
-        self.assertEqual(341, deck[3])
-        # This test will also check extract_detail function
-        self.assertEqual('Research Smoko', deck[4][0])
-        self.assertEqual('Logos', deck[5][0])
-        self.assertEqual('Creature', deck[6][0])
-        self.assertEqual(0, deck[7][0])
-        self.assertEqual('2', deck[8][0])
-        self.assertEqual('0', deck[9][0])
-        # There should be 30 cards in total in each deck
-        self.assertEqual(30, len(deck[5]))
+    def test_load_decks(self):
+        # Starting with deck 1, load 4 decks
+        decks = load_decks(1, 4)
+        # each load has 3 elements: 'data', 'count', '_linked'
+        self.assertEqual(3, len(decks))
+        # count the 4 decks
+        self.assertEqual(4, len(decks['data']))
 
     def test_total_decks(self):
         count = int(total_decks())
         self.assertTrue(count > 120000)
+
+    def test_decode_decks(self):
+        decks = load_decks(1, 1)
+        decks_df = decode_decks(decks)
+        # Need more checks here
+        True
+
+    def test_decode_cards(self):
+        decks = load_decks(1, 1)
+        cards_df = decode_cards(decks)
+        # Need more checks here
+        True
+
+###############################################################################
+# Code for when file is run from command line                                 #
+###############################################################################
 
 
 if __name__ == '__main__':
