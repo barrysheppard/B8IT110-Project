07b797b7359fe4850c9b9f02862d22f64f86566e
Barry Sheppard <barrysheppard@gmail.com>
2019-10-15 22:59:53 +0100

    updated unittests with templates for currently untested functions

--- test_clean.py
+++ test_clean.py
@@ -2,7 +2,7 @@
 ###############################################################################
 # Title: Keyforge Project                                                     #
 # Student: Barry Sheppard ID: 10387786                                        #
-# Task: Load keyforge decks from https://www.keyforgegame.com                 #
+# Task: clean keyforge decks from https://www.keyforgegame.com                #
 ###############################################################################
 
 
@@ -11,11 +11,8 @@
 ###############################################################################
 import unittest
 import pandas as pd
-from load import load_decks
-from load import total_decks
-from load import decode_cards
-from load import decode_decks
-
+from clean import create_card_df
+from clean import create_deck_df
 
 ###############################################################################
 # Functions                                                                   #
@@ -25,30 +22,14 @@ from load import decode_decks
 # test the load functionality
 class TestLoad(unittest.TestCase):
 
-    def test_load_decks(self):
-        # Starting with deck 1, load 4 decks
-        decks = load_decks(1, 4)
-        # each load has 3 elements: 'data', 'count', '_linked'
-        self.assertEqual(3, len(decks))
-        # count the 4 decks
-        self.assertEqual(4, len(decks['data']))
-
-    def test_total_decks(self):
-        count = int(total_decks())
-        self.assertTrue(count > 120000)
-
-    def test_decode_decks(self):
-        decks = load_decks(1, 1)
-        decks_df = decode_decks(decks)
-        #self.assertIsInstance(pd.DataFrame, type(decks_df))
+    def test_create_card_df(self):
         # Need more checks here
         True
 
-    def test_decode_cards(self):
-        decks = load_decks(1, 1)
-        cards_df = decode_cards(decks)
+    def test_create_deck_df(self):
         # Need more checks here
         True
+
 
 ###############################################################################
 # Code for when file is run from command line                                 #
