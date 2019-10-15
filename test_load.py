11cd1525541f4130646da7541cede04873581ccd
Barry Sheppard <barrysheppard@gmail.com>
2019-10-15 23:00:45 +0100

    updated unittests with templates for currently untested functions

--- test_load.py
+++ test_load.py
@@ -15,6 +15,8 @@ from load import load_decks
 from load import total_decks
 from load import decode_cards
 from load import decode_decks
+from load import load_bulk_decks
+from load import create_blank_csv
 
 
 ###############################################################################
@@ -24,6 +26,27 @@ from load import decode_decks
 
 # test the load functionality
 class TestLoad(unittest.TestCase):
+
+    def test_create_blank_csv(self):
+        # Need more checks here
+        True
+
+    def test_decode_cards(self):
+        decks = load_decks(1, 1)
+        cards_df = decode_cards(decks)
+        # Need more checks here
+        True
+
+    def test_decode_decks(self):
+        decks = load_decks(1, 1)
+        decks_df = decode_decks(decks)
+        #self.assertIsInstance(pd.DataFrame, type(decks_df))
+        # Need more checks here
+        True
+
+    def test_load_bulk_decks(self):
+        # Need more checks here
+        True
 
     def test_load_decks(self):
         # Starting with deck 1, load 4 decks
@@ -37,18 +60,6 @@ class TestLoad(unittest.TestCase):
         count = int(total_decks())
         self.assertTrue(count > 120000)
 
-    def test_decode_decks(self):
-        decks = load_decks(1, 1)
-        decks_df = decode_decks(decks)
-        #self.assertIsInstance(pd.DataFrame, type(decks_df))
-        # Need more checks here
-        True
-
-    def test_decode_cards(self):
-        decks = load_decks(1, 1)
-        cards_df = decode_cards(decks)
-        # Need more checks here
-        True
 
 ###############################################################################
 # Code for when file is run from command line                                 #
